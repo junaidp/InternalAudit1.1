1c0aa9e11af1a479fdb70236cc21419259a9825b
junaid paracha <junaidp@gmail.com>
2018-03-05 11:45:11 +0500

    feedback help

---
+++ src/com/internalaudit/shared/Feedback.java
@@ -0,0 +1,66 @@
+package com.internalaudit.shared;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+
+@Table(name="feedback")
+public class Feedback   implements Serializable {
+
+	@Id 
+	@Column(name="id")
+	private int id;
+	
+	@Column(name ="heading")
+	private String heading;
+	
+	@Column(name ="description")
+	private String description;
+	
+	@JoinColumn(name = "employee")
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Employee employee;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getHeading() {
+		return heading;
+	}
+
+	public void setHeading(String heading) {
+		this.heading = heading;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	
+}
